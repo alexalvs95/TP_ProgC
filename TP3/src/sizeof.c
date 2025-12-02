ab85e51da360ca8770dfccbd1b9c07d04fd9f60d
alexalvs95 <154414622+alexalvs95@users.noreply.github.com>
2025-12-02 11:16:19 +0100

    Update sizeof.c

---
+++ TP3/src/sizeof.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("La taille de int est : %zu octets\n", sizeof(int));
+    printf("La taille de int* est : %zu octets\n", sizeof(int*));
+    printf("La taille de int** est : %zu octets\n", sizeof(int**));
+
+    printf("La taille de char est : %zu octets\n", sizeof(char));
+    printf("La taille de char* est : %zu octets\n", sizeof(char*));
+    printf("La taille de char** est : %zu octets\n", sizeof(char**));
+    printf("La taille de char*** est : %zu octets\n", sizeof(char***));
+
+    printf("La taille de float est : %zu octets\n", sizeof(float));
+    printf("La taille de float* est : %zu octets\n", sizeof(float*));
+    printf("La taille de float** est : %zu octets\n", sizeof(float**));
+    printf("La taille de float*** est : %zu octets\n", sizeof(float***));
+
+    printf("La taille de double est : %zu octets\n", sizeof(double));
+    printf("La taille de double* est : %zu octets\n", sizeof(double*));
+
+    return 0;
+}
